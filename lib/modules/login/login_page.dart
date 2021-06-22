bf4961513c7f8fdf2d84907c887bb3aa66af7c61
emilioheinz <emiliosheinz@gmail.com>
2021-06-21 21:54:32 -0300

    feat: login page layout

--- lib/modules/login/login_page.dart
+++ lib/modules/login/login_page.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:payflow/shared/themes/app_colors.dart';
+import 'package:payflow/shared/themes/app_images.dart';
+import 'package:payflow/shared/themes/app_text_style.dart';
+import 'package:payflow/shared/widgets/social_login/social_login_button.dart';
 
 class LoginPage extends StatefulWidget {
   LoginPage({Key? key}) : super(key: key);
@@ -12,6 +15,61 @@ class LoginPage extends StatefulWidget {
 class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(backgroundColor: AppColors.background, body: Stack());
+    final screenSize = MediaQuery.of(context).size;
+    final topContainerHeight = screenSize.height * 0.35;
+
+    return Scaffold(
+      backgroundColor: AppColors.background,
+      body: Container(
+        height: double.infinity,
+        width: double.infinity,
+        child: Stack(
+          children: [
+            Container(
+              color: AppColors.primary,
+              width: double.infinity,
+              height: topContainerHeight,
+            ),
+            Positioned(
+              top: topContainerHeight / 5,
+              left: 0,
+              right: 0,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Image.asset(
+                    AppImages.person,
+                    width: 208,
+                    height: 300,
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(top: 20),
+                    child: Image.asset(AppImages.logomini),
+                  ),
+                  Padding(
+                    padding:
+                        const EdgeInsets.only(left: 70, right: 70, top: 20),
+                    child: Text(
+                      "Organize seus boletos em um só lugar",
+                      textAlign: TextAlign.center,
+                      style: AppTextStyles.titleHome,
+                    ),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.all(40),
+                    child: SocialLoginButton(
+                      onTap: () {
+                        print("TODO emilioheinz: call google login method");
+                      },
+                    ),
+                  )
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
