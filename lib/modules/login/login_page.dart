d552ef656ac14425da54eeedf0d857df5e9fe55d
emilioheinz <emiliosheinz@gmail.com>
2021-06-22 21:48:38 -0300

    feat: configure google signin with firebase

--- lib/modules/login/login_page.dart
+++ lib/modules/login/login_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 import 'package:payflow/shared/themes/app_colors.dart';
 import 'package:payflow/shared/themes/app_images.dart';
 import 'package:payflow/shared/themes/app_text_style.dart';
@@ -59,8 +60,17 @@ class _LoginPageState extends State<LoginPage> {
                   Padding(
                     padding: EdgeInsets.all(40),
                     child: SocialLoginButton(
-                      onTap: () {
-                        print("TODO emilioheinz: call google login method");
+                      onTap: () async {
+                        GoogleSignIn _googleSignIn = GoogleSignIn(
+                          scopes: ['email'],
+                        );
+
+                        try {
+                          final response = await _googleSignIn.signIn();
+                          print(response);
+                        } catch (error) {
+                          print(error);
+                        }
                       },
                     ),
                   )
